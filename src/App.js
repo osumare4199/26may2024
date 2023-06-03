0b0aebeacdca49a5dd254a4aa18d8dac1ac64134
emmanuel <132175837+osumare4199@users.noreply.github.com>
2023-06-03 03:09:27 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./styles.css";
 import Navbar from "./components/Navbar";
 import { Routes } from "react-router-dom";
+import Home from "./routes/Home";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home/>
+      
     </div>
   );
 }
