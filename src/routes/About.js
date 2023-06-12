5070fbfa0526c0522a309b1b718c18c64228d642
emmanuel <132175837+osumare4199@users.noreply.github.com>
2023-06-12 01:09:42 +0000

    Update HeroStyles.css and About.js

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,22 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar"
+import AboutImg from "../assets/person-sits-looking-out-towards-still-water.jpg"
+
 function about (){
     return(
         <>
-        <h1>This is about</h1>
-        </>
+         <Navbar />
+        <Hero
+        cName=".hero-mid"
+        heroImg={AboutImg}
+        title="Your Journey Your Story"
+        
+       
+        url="/"
+        btnClass="hide"
+        />
+        </> 
+    
     )
 }
 
